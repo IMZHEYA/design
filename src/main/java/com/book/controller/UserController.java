f0cf76586cf42f6562ca39bced0e082dfa69b7e2
Serendipityzhezi <1342558165@qq.com>
2024-07-11 15:37:56 +0800

    feat:🙂初始化传统登录，注册功能

---
+++ src/main/java/com/book/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.book.controller;
+
+import com.book.pojo.UserInfo;
+import com.book.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @PostMapping("/login")
+    public String login(String account,String password){
+        return userService.login(account,password);
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody UserInfo userInfo){
+        return userService.register(userInfo);
+    }
+
+}
---
+++ src/main/java/com/book/repo/UserRepository.java
@@ -0,0 +1,14 @@
+package com.book.repo;
+
+import com.book.pojo.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserInfo,Integer> {
+    //根据用户名查询用户信息
+    UserInfo findByUserName(String userName);
+
+    //根据用户和密码查询用户信息
+    UserInfo findByUserNameAndUserPassword(String account,String password);
+}
