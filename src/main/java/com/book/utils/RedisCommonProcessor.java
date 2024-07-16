4217dc82b106065c7f9d1b2d73e588ccc4e00b9c
Serendipityzhezi <1342558165@qq.com>
2024-07-16 10:39:59 +0800

    feat:🙂添加redis工具类，对redis常用操作进行封装

---
+++ src/main/java/com/book/utils/RedisCommonProcessor.java
@@ -0,0 +1,33 @@
+package com.book.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisCommonProcessor {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    //通过key获取value
+    public Object get(String key){
+        if(key == null){
+            throw new UnsupportedOperationException("Redis key could not be null");
+        }
+        return redisTemplate.opsForValue().get(key);
+    }
+    //向redis中存入k v 键值对
+    public void set(String key,Object value){
+        redisTemplate.opsForValue().set(key,value);
+    }
+    //向redis中存入k v 数据对，并支持过期时间
+    public void set(String key,Object value,long time){
+        if(time > 0){
+            redisTemplate.opsForValue().set(key,value,time, TimeUnit.SECONDS);
+        }else {
+            set(key, value);
+        }
+    }
+}
