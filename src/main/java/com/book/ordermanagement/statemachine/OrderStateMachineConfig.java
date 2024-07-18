be653d4ea104871cf9cf890f66e42acc586b866c
Serendipityzhezi <1342558165@qq.com>
2024-07-18 14:49:28 +0800

    feat🙂:状态模式实战-引入spring-statemachine-redis依赖对状态机持久化

---
+++ src/main/java/com/book/ordermanagement/statemachine/OrderStateMachineConfig.java
@@ -0,0 +1,43 @@
+package com.book.ordermanagement.statemachine;
+
+import com.book.ordermanagement.state.OrderState;
+import com.book.ordermanagement.state.OrderStateChangeAction;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachine(name = "OrderStateMachine")
+public class OrderStateMachineConfig extends StateMachineConfigurerAdapter<OrderState, OrderStateChangeAction> {
+
+   //覆写configure方法，进行状态机的初始化操作
+    @Override
+    public void configure(StateMachineStateConfigurer<OrderState, OrderStateChangeAction> states) throws Exception {
+        //设置订单创建成功后的初始化状态为待支付 ORDER_WAIT_PAY
+        states.withStates().initial(OrderState.ORDER_WAIT_PAY)
+                //将订单状态类 OrderState 中所有的状态，加载配置到状态机中
+                .states(EnumSet.allOf(OrderState.class));
+    }
+
+    //配置订单状态的转换流程
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<OrderState, OrderStateChangeAction> transitions) throws Exception {
+        transitions.withExternal()
+                .source(OrderState.ORDER_WAIT_PAY)
+                .target(OrderState.ORDER_WAIT_SEND)
+                .event(OrderStateChangeAction.PAY_ORDER)
+                .and()
+                .withExternal().source(OrderState.ORDER_WAIT_SEND)
+                .target(OrderState.ORDER_WAIT_RECEIVE)
+                .event(OrderStateChangeAction.SEND_ORDER)
+                .and()
+                .withExternal().source(OrderState.ORDER_WAIT_RECEIVE)
+                .target(OrderState.ORDER_FINISH)
+                .event(OrderStateChangeAction.RECEIVE_ORDER);
+    }
+}
