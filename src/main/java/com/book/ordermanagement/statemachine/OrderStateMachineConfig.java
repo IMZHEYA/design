3ffcca72af68d6758391942aa7629e491cedee6d
Serendipityzhezi <1342558165@qq.com>
2024-07-18 15:18:53 +0800

    feat🙂:状态模式实战-spring状态机配置类实战

--- src/main/java/com/book/ordermanagement/statemachine/OrderStateMachineConfig.java
+++ src/main/java/com/book/ordermanagement/statemachine/OrderStateMachineConfig.java
@@ -2,11 +2,18 @@ package com.book.ordermanagement.statemachine;
 
 import com.book.ordermanagement.state.OrderState;
 import com.book.ordermanagement.state.OrderStateChangeAction;
+import org.hibernate.annotations.Persister;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.persist.RepositoryStateMachinePersist;
+import org.springframework.statemachine.redis.RedisStateMachineContextRepository;
+import org.springframework.statemachine.redis.RedisStateMachinePersister;
 
 import java.util.EnumSet;
 
@@ -14,6 +21,8 @@ import java.util.EnumSet;
 @EnableStateMachine(name = "OrderStateMachine")
 public class OrderStateMachineConfig extends StateMachineConfigurerAdapter<OrderState, OrderStateChangeAction> {
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
    //覆写configure方法，进行状态机的初始化操作
     @Override
     public void configure(StateMachineStateConfigurer<OrderState, OrderStateChangeAction> states) throws Exception {
@@ -40,4 +49,13 @@ public class OrderStateMachineConfig extends StateMachineConfigurerAdapter<Order
                 .target(OrderState.ORDER_FINISH)
                 .event(OrderStateChangeAction.RECEIVE_ORDER);
     }
+
+    //初始化RedisStateMachinePersister
+    @Bean(name = "stateMachineRedisPersister")
+    public RedisStateMachinePersister<OrderState,OrderStateChangeAction> getRedisPersister(){
+        RedisStateMachineContextRepository<OrderState, OrderStateChangeAction> repository = new RedisStateMachineContextRepository<>(redisConnectionFactory);
+        RepositoryStateMachinePersist<OrderState, OrderStateChangeAction> persist = new RepositoryStateMachinePersist<>(repository);
+        return new RedisStateMachinePersister<>(persist);
+    }
+
 }
